ba15f1f0e28ac03d5c513de3716709f940c9e0af
Zydrunas-Sir <73161629+Zydrunas-Sir@users.noreply.github.com>
2021-02-01 09:39:36 +0200

    Fixed up create functions

--- Task2/task2.py
+++ Task2/task2.py
@@ -3,13 +3,13 @@ from Task2.DatabaseManager import DatabaseContextManager
 
 def create_table_transactions():
     query = """CREATE TABLE `transactions` (
-    `id` integer,
+    `id` integer NOT NULL AUTO_INCREMENT,
     `from_account_id` integer,
     `to_account_id` integer,
     `amount` decimal(5, 2),
     PRIMARY KEY (id),
-    FOREIGN KEY (from_account_id) REFERENCES transactions(id),
-    FOREIGN KEY (to_account_id) REFERENCES transactions(id));"""
+    FOREIGN KEY (from_account_id) REFERENCES account(id),
+    FOREIGN KEY (to_account_id) REFERENCES account(id));"""
     with DatabaseContextManager() as db:
         cursor = db.cursor()
         cursor.execute(query)
@@ -17,7 +17,7 @@ def create_table_transactions():
 
 def create_table_account():
     query = """CREATE TABLE `account` (
-    `id` integer,
+    `id` integer NOT NULL AUTO_INCREMENT,
     `first_name` varchar(255),
     `last_name` varchar(255),
     `balance` decimal(5, 2),
@@ -41,7 +41,7 @@ def transaction():
             cursor = db.cursor()
             cursor.execute(query, query1)
 
-    except cursor.connector.Error as error:
+    except db.connector.Error as error:
         print("Failed to update record to database rollback: {}".format(error))
         db.rollback()
     finally:
@@ -51,16 +51,16 @@ def transaction():
 
 
 def create_transaction(from_account_id: int, to_account_id: int, amount: float):
-    query = f"""INSERT INTO transactions(from_account_id, to_account_id, amount) VALUES(?, ?, ?)"""
+    query = f"""INSERT INTO transactions(from_account_id, to_account_id, amount) VALUES(%s, %s, %s)"""
     parameters = [from_account_id, to_account_id, amount]
-    with DatabaseContextManager("db") as db:
+    with DatabaseContextManager() as db:
         cursor = db.cursor()
         cursor.execute(query, parameters)
 
 
 def create_account(first_name: str, last_name: str, balance: float):
-    query = f"""INSERT INTO account(first_name, last_name, balance) VALUES(?, ?, ?)"""
+    query = f"""INSERT INTO account(first_name, last_name, balance) VALUES(%s, %s, %s)"""
     parameters = [first_name, last_name, balance]
-    with DatabaseContextManager("db") as db:
+    with DatabaseContextManager() as db:
         cursor = db.cursor()
         cursor.execute(query, parameters)
